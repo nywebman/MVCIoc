20ef3183443f514e7e8788297187b4d7a061fa89
Brad Rosen <brosen@mdsol.com>
2014-12-24 14:26:05 -0500

    initial

---
+++ MVCIoc.Tests/Controllers/ProteinTrackerControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCIoc.Controllers;
+
+namespace MVCIoc.Tests.Controllers
+{
+    [TestClass]
+    public class ProteinTrackerControllerTest
+    {
+        [TestMethod]
+        public void WhenNothingHasHappendTotalAndGoalAreZero()
+        {
+            ProteinTrackerController controller=new ProteinTrackerController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.AreEqual(0,result.ViewBag.Total);
+            Assert.AreEqual(0, result.ViewBag.Goal);
+        }
+    }
+}
---
+++ MVCIoc/Models/ProteinTrackingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIoc.Models
+{
+    public class ProteinTrackingService 
+    {
+
+        public int Total { get; set; }
+
+        public int Goal { get; set; }
+
+        public void AddProtein(int amount)
+        {
+            Total += amount;
+        }
+
+    }
+}
