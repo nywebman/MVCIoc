ce6be14c4cf322dc71bce37963358179e9053dbb
Brad Rosen <brosen@mdsol.com>
2014-12-29 13:36:05 -0500

    remove unity install ninject

--- MVCIoc/Global.asax.cs
+++ MVCIoc/Global.asax.cs
@@ -12,8 +12,6 @@ namespace MVCIoc
     {
         protected void Application_Start()
         {
-            Bootstrapper.Initialise();
-
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
--- MVCIoc/Pages/ProteinTrackerBasePage.cs
+++ MVCIoc/Pages/ProteinTrackerBasePage.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.Practices.Unity;
 using MVCIoc.Models;
 
 namespace MVCIoc.Pages
 {
     public class ProteinTrackerBasePage :WebViewPage
     {
-        [Dependency]
         public IAnalyticService AnalyticService { get; set; }
 
         //need parameterless contructor
