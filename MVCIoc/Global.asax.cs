eb90b92cf8aa8633884eaa719365e2f896857a70
Brad Rosen <brosen@mdsol.com>
2014-12-29 14:12:42 -0500

    install simple injector

--- MVCIoc/Global.asax.cs
+++ MVCIoc/Global.asax.cs
@@ -3,8 +3,6 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Autofac;
-using Autofac.Integration.Mvc;
 using MVCIoc.Filters;
 using MVCIoc.Models;
 using MVCIoc.Pages;
@@ -18,8 +16,6 @@ namespace MVCIoc
     {
         protected void Application_Start()
         {
-            RegisterAutoFac();
-
             AreaRegistration.RegisterAllAreas();
 
            // WebApiConfig.Register(GlobalConfiguration.Configuration);
@@ -27,26 +23,6 @@ namespace MVCIoc
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
-        }
-
-        private void RegisterAutoFac()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterControllers(Assembly.GetExecutingAssembly());
-            builder.RegisterSource(new ViewRegistrationSource());
-
-            builder.RegisterType<DebugMessageService>().As<IDebugMessageService>();
-            builder.RegisterType<DebugFilter>();                                     //so filter will work
-            builder.RegisterType<AnalyticService>().As<IAnalyticService>();
-            builder.RegisterType<ProteinTrackerBasePage>().PropertiesAutowired();   //so property injection works, no attrib needed on ProteinTrackerBasePage
-            builder.RegisterType<ProteinTrackingService>().As<IProteinTrackingService>();
-            builder.RegisterType<ProteinRepository>().As<IProteinRepository>();
-
-            var container = builder.Build();
-
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-
-
         }
     }
 }
