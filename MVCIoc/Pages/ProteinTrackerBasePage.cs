8680fb686e3af1dfbafdb30aea8cd31f874ba8b6
Brad Rosen <brosen@mdsol.com>
2014-12-29 13:44:23 -0500

    uninstall Ninject install structuremap

--- MVCIoc/Pages/ProteinTrackerBasePage.cs
+++ MVCIoc/Pages/ProteinTrackerBasePage.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCIoc.Models;
-using Ninject;
 
 namespace MVCIoc.Pages
 {
     public class ProteinTrackerBasePage :WebViewPage
     {
-        [Inject]
         public IAnalyticService AnalyticService { get; set; }
 
         public override void Execute()
