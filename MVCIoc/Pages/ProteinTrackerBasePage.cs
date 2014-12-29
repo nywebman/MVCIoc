d125a899e1634e153fbbedc2f9085209ca97a58c
Brad Rosen <brosen@mdsol.com>
2014-12-29 13:45:27 -0500

    configure structuremap

--- MVCIoc/Pages/ProteinTrackerBasePage.cs
+++ MVCIoc/Pages/ProteinTrackerBasePage.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCIoc.Models;
+using StructureMap.Attributes;
 
 namespace MVCIoc.Pages
 {
     public class ProteinTrackerBasePage :WebViewPage
     {
+        [SetterProperty]
         public IAnalyticService AnalyticService { get; set; }
 
         public override void Execute()
