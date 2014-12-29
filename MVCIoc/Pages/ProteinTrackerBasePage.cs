21a7441b0d535257b958e188bbc3762f4d05c049
Brad Rosen <brosen@mdsol.com>
2014-12-29 13:49:21 -0500

    remove Structuremap install autofac

--- MVCIoc/Pages/ProteinTrackerBasePage.cs
+++ MVCIoc/Pages/ProteinTrackerBasePage.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCIoc.Models;
-using StructureMap.Attributes;
 
 namespace MVCIoc.Pages
 {
     public class ProteinTrackerBasePage :WebViewPage
     {
-        [SetterProperty]
         public IAnalyticService AnalyticService { get; set; }
 
         public override void Execute()
